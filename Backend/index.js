912ae855be21ba8f58c6662a9c7ac297c61549dc
Rajendar <raa.singh0000@gmail.com>
2024-04-16 04:52:44 +0530

    database connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.DATABASE_LINK)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("app is listing on port 3000");
