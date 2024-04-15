2123be97ca6e038211143367e86e59515c960bf1
Rajendar <raa.singh0000@gmail.com>
2024-04-16 03:53:49 +0530

    first commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Components Import
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import { Header } from "./componentes/Header.jsx";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/singin" element={<SignIn />} />
+        <Route path="/singup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
